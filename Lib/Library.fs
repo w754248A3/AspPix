060928d096a0851c27b4c93f2c1c1cbac0a5ad7d
LeiKaiFeng-GoodBoy <1441754263@qq.com>
2021-12-12 15:50:29 +0800

    all

---
+++ Lib/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace AspPix
+
+module Pix =
+
+    
---
+++ TestFsTest/Program.fs
@@ -0,0 +1,41 @@
+﻿open System
+open System.Net.Http
+open System.Threading.Tasks
+open System.Threading
+open FSharp.Core
+open System
+open System.IO
+open System.Linq
+open FSharp.Control
+open System.Security.Cryptography
+open System.Text
+open FSharp.NativeInterop
+open System.Buffers
+open System.Net.Sockets
+open System.Security.Authentication
+open System.Net.Security
+open System.Net
+open System.Net.Http.Headers
+
+open System.Text.RegularExpressions
+
+[<EntryPoint>]
+let main argv =
+    
+    TaskScheduler.UnobservedTaskException.Add(fun evArgs -> Console.WriteLine(evArgs))
+
+
+
+    Task.FromException(new ArgumentException())|> ignore
+
+
+    GC.Collect();
+    GC.Collect();
+    GC.Collect();
+    GC.Collect();
+
+
+    while true do
+        ()
+    Console.ReadLine()|>ignore
+    0

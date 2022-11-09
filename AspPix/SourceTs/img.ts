7ff34ef0e902eedd0ede3a4296ab6bdd4c9ae33a
LeiKaiFeng-GoodBoy <1441754263@qq.com>
2022-11-09 15:09:03 +0800

    all

--- AspPix/SourceTs/img.ts
+++ AspPix/SourceTs/img.ts
@@ -1,34 +1,87 @@
 (function () {
     document.addEventListener("DOMContentLoaded", function () {
 
+        (function (){
 
-        let imgDiv = <HTMLDivElement>document.getElementById("imgDiv");
+            let imgDiv = <HTMLDivElement>document.getElementById("imgDiv");
 
-        let vs = JSON.parse(window.atob(imgDiv.getAttribute("my")));
+            let vs = JSON.parse(window.atob(imgDiv.getAttribute("my")));
+    
+    
+            function addImg(vs: string[], n: number) {
+    
+                if (vs.length !== 0) {
+    
+                    let uri = vs[0] + n + vs[1];
+    
+                    let img = document.createElement("img");
+    
+                    img.onload = () => {
+    
+                        if (img.width !== 0 && img.height !== 0) {
+                            imgDiv.appendChild(img);
+                            addImg(vs, n + 1);
+                        }
+                    };
+    
+                    img.src = uri;
+    
+                    
+                }
+            }
+    
+            addImg(vs, 0);
+
+        })();
 
 
-        function addImg(vs: string[], n: number) {
 
-            if (vs.length !== 0) {
+        (function(){
 
-                let uri = vs[0] + n + vs[1];
+            function request(s:string){
+                let xml = new XMLHttpRequest();
 
-                let img = document.createElement("img");
+                xml.addEventListener("loadend", ()=> window.location.reload());
 
-                img.onload = () => {
+                xml.open("GET", s);
+                xml.send(null);
 
-                    if (img.width !== 0 && img.height !== 0) {
-                        imgDiv.appendChild(img);
-                        addImg(vs, n + 1);
-                    }
-                };
+            }
 
-                img.src = uri;
+
+            let a = <HTMLAnchorElement>document.querySelector("body > div > div:nth-child(4) > a");
+            let par = a.parentElement;
+
+            a.remove();
+
+            let input = document.createElement("input");
+
+            input.type = "checkbox";
+            par.appendChild(input);
+            if(a.innerText !== "不喜欢"){
+                
+                
+                input.checked=true;
 
                 
             }
-        }
 
-        addImg(vs, 0);
+
+            input.addEventListener("change", (ev)=>{
+                console.log(input.checked);
+                if(input.checked){
+                    if(a.innerText !== "不喜欢"){
+                        request(a.href);
+                    }
+                }
+                else{
+                    if(a.innerText === "不喜欢"){
+                        request(a.href);
+                    }
+                }
+
+            });
+        })();
+        
     });
 })();

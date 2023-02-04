fabde14882c6974e43e1d9255a50f90d5f65bf57
猪无能 <798158053@qq.com>
2023-02-04 16:38:45 +0800

    更改为NET7

--- AspPix/wwwroot/pix/js/file.js
+++ AspPix/wwwroot/pix/js/file.js
@@ -1,53 +1,107 @@
 (function () {
+    function set输入部分的HTML为固定布局(form, imgs) {
+        let faterstyle = form.parentElement.style;
+        faterstyle.position = "fixed";
+        faterstyle.left = "0";
+        faterstyle.top = "0";
+        faterstyle.backgroundColor = "White";
+        imgs.style.marginTop = form.offsetHeight + "px";
+    }
+    function getUriPathWithoutQuery() {
+        return window.location.href.split('?')[0];
+    }
+    function on当元素进入窗口时运行(e, func) {
+        let options = {
+            rootMargin: '0px',
+            threshold: 1.0
+        };
+        let observer = new IntersectionObserver((entries, observer) => {
+            let entrie = entries.filter(v => v.target === e)[0];
+            if (entrie && entrie.isIntersecting) {
+                observer.disconnect();
+                setTimeout(func, 0);
+            }
+        }, options);
+        observer.observe(e);
+    }
+    function load新的图片(uri, func) {
+        let xml = new XMLHttpRequest();
+        xml.responseType = "document";
+        xml.addEventListener("loadend", () => {
+            let doc = xml.response;
+            setTimeout(() => func(doc), 0);
+        });
+        xml.open("GET", uri);
+        xml.send(null);
+    }
+    function createUri(form) {
+        let fd = new FormData(form);
+        let uq = new URLSearchParams(fd);
+        return getUriPathWithoutQuery() + "?" + uq.toString();
+    }
+    function into将图片放入DOM(imgs, doc, func) {
+        let e = doc.getElementById("imgs");
+        let newimgs = e.getElementsByTagName("a");
+        if (newimgs.length !== 0) {
+            let last = newimgs[0];
+            let coll = new DocumentFragment();
+            coll.append(...newimgs);
+            imgs.appendChild(coll);
+            setTimeout(() => func(last), 0);
+        }
+    }
+    function run增量加载图片(imgs, form, pageCount) {
+        let n = 0;
+        function on进入窗口CallBack() {
+            pageCount.value = (++n).toString();
+            let uri = createUri(form);
+            load新的图片(uri, (doc) => {
+                into将图片放入DOM(imgs, doc, (last) => {
+                    on当元素进入窗口时运行(last, () => {
+                        on进入窗口CallBack();
+                    });
+                });
+            });
+        }
+        on进入窗口CallBack();
+    }
+    function set图片样式() {
+        let sy = document.createElement("style");
+        sy.innerHTML = `
+
+            body {
+
+                background-color: black;
+
+            }
+
+            img {
+
+                object-fit: contain;
+
+                height: 200px;
+
+                width: 200px;
+
+
+            }
+
+        `;
+        document.head.appendChild(sy);
+    }
+    set图片样式();
     document.addEventListener("DOMContentLoaded", function () {
         let getForm = document.getElementById("form");
         let pageCount = document.getElementById("down");
         let imgs = document.getElementById("imgs");
         let last = document.getElementById("last");
-        function getUriPathWithoutQuery() {
-            return window.location.href.split('?')[0];
-        }
-        let faterstyle = getForm.parentElement.style;
-        faterstyle.position = "fixed";
-        faterstyle.left = "0";
-        faterstyle.top = "0";
-        faterstyle.backgroundColor = "White";
-        imgs.style.marginTop = getForm.offsetHeight + "px";
+        set输入部分的HTML为固定布局(getForm, imgs);
+        run增量加载图片(imgs, getForm, pageCount);
         getForm.addEventListener("change", (ev) => {
             if (ev.target !== pageCount) {
                 pageCount.value = "0";
             }
             getForm.submit();
         });
-        let options = {
-            rootMargin: '0px',
-            threshold: 1.0
-        };
-        let cn = 0;
-        let observer = new IntersectionObserver((entries, observer) => {
-            if (!entries.filter(v => v.target === last)[0]) {
-                return;
-            }
-            pageCount.value = (++cn).toString();
-            let xml = new XMLHttpRequest();
-            xml.responseType = "document";
-            xml.addEventListener("loadend", () => {
-                let doc = xml.response;
-                let e = doc.getElementById("imgs");
-                let newimgs = e.getElementsByTagName("a");
-                if (newimgs.length === 0) {
-                    observer.disconnect();
-                }
-                else {
-                    imgs.append(...newimgs);
-                }
-            });
-            let fd = new FormData(getForm);
-            let map = new Map(fd);
-            let uq = new URLSearchParams(map);
-            xml.open("GET", getUriPathWithoutQuery() + "?" + uq.toString());
-            xml.send();
-        }, options);
-        observer.observe(last);
     });
 })();
--- AspPix/wwwroot/pix/js/img.js
+++ AspPix/wwwroot/pix/js/img.js
@@ -1,20 +1,52 @@
 (function () {
     document.addEventListener("DOMContentLoaded", function () {
-        let imgDiv = document.getElementById("imgDiv");
-        let vs = JSON.parse(window.atob(imgDiv.getAttribute("my")));
-        function addImg(vs, n) {
-            if (vs.length !== 0) {
-                let uri = vs[0] + n + vs[1];
-                let img = document.createElement("img");
-                img.onload = () => {
-                    if (img.width !== 0 && img.height !== 0) {
-                        imgDiv.appendChild(img);
-                        addImg(vs, n + 1);
-                    }
-                };
-                img.src = uri;
+        (function () {
+            let imgDiv = document.getElementById("imgDiv");
+            let vs = JSON.parse(window.atob(imgDiv.getAttribute("my")));
+            function addImg(vs, n) {
+                if (vs.length !== 0) {
+                    let uri = vs[0] + n + vs[1];
+                    let img = document.createElement("img");
+                    img.onload = () => {
+                        if (img.width !== 0 && img.height !== 0) {
+                            imgDiv.appendChild(img);
+                            addImg(vs, n + 1);
+                        }
+                    };
+                    img.src = uri;
+                }
             }
-        }
-        addImg(vs, 0);
+            addImg(vs, 0);
+        })();
+        (function () {
+            function request(s) {
+                let xml = new XMLHttpRequest();
+                xml.addEventListener("loadend", () => window.location.reload());
+                xml.open("GET", s);
+                xml.send(null);
+            }
+            let a = document.querySelector("body > div > div:nth-child(4) > a");
+            let par = a.parentElement;
+            a.remove();
+            let input = document.createElement("input");
+            input.type = "checkbox";
+            par.appendChild(input);
+            if (a.innerText !== "不喜欢") {
+                input.checked = true;
+            }
+            input.addEventListener("change", (ev) => {
+                console.log(input.checked);
+                if (input.checked) {
+                    if (a.innerText !== "不喜欢") {
+                        request(a.href);
+                    }
+                }
+                else {
+                    if (a.innerText === "不喜欢") {
+                        request(a.href);
+                    }
+                }
+            });
+        })();
     });
 })();
